b98257af54c4883894e51384e33dc29eee79b26d
Kehinde Egunjobi <kehindeegunjobi93@yahoo.com>
2018-11-13 08:42:17 +0100

    Updates

---
+++ precache-manifest.630d392e211dec5b50ea3ec2a71213e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "869507bc81f7af9c1e32",
+    "url": "/react-redux-movie-database/static/js/runtime~main.869507bc.js"
+  },
+  {
+    "revision": "5ee8698a36b32c8a07fd",
+    "url": "/react-redux-movie-database/static/js/main.5ee8698a.chunk.js"
+  },
+  {
+    "revision": "f21e997f6f24d578ec44",
+    "url": "/react-redux-movie-database/static/js/1.f21e997f.chunk.js"
+  },
+  {
+    "revision": "5ee8698a36b32c8a07fd",
+    "url": "/react-redux-movie-database/static/css/main.e18a03e1.chunk.css"
+  },
+  {
+    "revision": "88c3e9ce7e58e0bcbbbb09e4bbd21e3b",
+    "url": "/react-redux-movie-database/index.html"
+  }
+];
